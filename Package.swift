70dc95d37168f16103946c95eee4992a2b270d08
viachpaliy <viachpaliy@ukr.net>
2018-06-17 10:30:55 +0300

    change repository from original to fork

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GLibObject",
     dependencies: [
-        .Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftGLib.git", majorVersion: 2)
     ],
     swiftLanguageVersions: [3, 4]
 )
