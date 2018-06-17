71438c2bd16fb044d9bd28f9fdfec07a521148e8
viachpaliy <viachpaliy@ukr.net>
2018-06-17 14:34:24 +0300

    change version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GLibObject",
     dependencies: [
-        .Package(url: "https://github.com/viachpaliy/SwiftGLib.git", majorVersion: 2)
+        .Package(url: "https://github.com/viachpaliy/SwiftGLib.git", majorVersion: 1)
     ],
     swiftLanguageVersions: [3, 4]
 )
